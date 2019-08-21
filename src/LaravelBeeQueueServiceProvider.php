d45af0a40b17d4bc109f671f8b4d9d9a02ce4c57
Stas <kapilka06@gmail.com>
2019-08-21 14:40:03 +0300

    LaravelBeeQueueServiceProvider

---
+++ src/LaravelBeeQueueServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Stasyanko\LaravelBeeQueue;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBeeQueueServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
